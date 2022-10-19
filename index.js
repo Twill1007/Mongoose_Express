4027cc53ae5417bb28ef86898eb861df712c441d
Twill1007 <thomaswillford@gmail.com>
2022-10-19 16:41:56 -0700

    Farm Stand App 10/19 441pm

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(methodOverride('_method'));
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
-mongoose.connect('mongodb://localhost:27017/farmStand2', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/farmStandtake2', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connection open!!')
     })
@@ -28,23 +28,37 @@ app.get('/products', async (req, res) => {
     const products = await Product.find({})
     console.log(products)
     res.render('products/index', { products })
-})
+});
 
 app.get('/products/new', (req, res) => {
     res.render('products/new', { categories })
+});
+
+//Farm Routes
+
+app.get('/farms/new', (req, res) => {
+    res.render('/farms/new')
 })
+
+
+
+//Product Routes
+
+
+
+
 
 app.post('/products', async (req, res) => {
     const newProduct = new Product(req.body);
     await newProduct.save();
     res.redirect(`/products/${newProduct._id}`)
-})
+});
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/show', { product })
-})
+});
 
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
@@ -57,6 +71,12 @@ app.put('/products/:id', async (req, res) => {
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
     res.redirect(`/products/${product._id}`)
 });
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
 
 app.listen('3000', () => {
     console.log('Listening on port 3000')
